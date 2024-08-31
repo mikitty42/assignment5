91b85b94a3422f784c8c76d40a61420de40e531f
takezawamiki <mikiaaamiki@yahoo.co.jp>
2024-08-31 16:22:41 +0900

    Implementation of editing screen

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,7 +19,18 @@ class PicturesController < ApplicationController
   end
 
   def edit
+      @picture = Picture.find(params[:id])
   end
+
+  def update
+      @picture = Picture.find(params[:id])
+      if @picture.update(picture_params)
+          redirect_to pictures_path
+      else
+          render :edit, status: :unprocessable_entity
+      end
+  end
+      
 
   def show
   end
