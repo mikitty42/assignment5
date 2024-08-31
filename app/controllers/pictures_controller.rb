134fceb870760b49c24f0a55202f52e1bc3eaca7
takezawamiki <mikiaaamiki@yahoo.co.jp>
2024-08-31 16:15:00 +0900

    Implementation of new registration screen

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,11 +3,30 @@ class PicturesController < ApplicationController
   end
 
   def new
+      @picture = Picture.new
+  end
+  
+  def create
+      @picture = Picture.new(picture_params)
+      if @picture.save
+          redirect_to pictures_path
+      else
+          render :new, status: :unprocessable_entity
+      end
+  end
+              
   end
 
   def edit
   end
 
   def show
+  end
+  
+  
+  private
+  
+  def picture
+      params.require(:picture).permit(:content,:image)
   end
 end
